1579ace17347ff980e2dc13600174e22e45be4ba
Robert C <robert.clarady@gmail.com>
2021-12-17 17:00:52 -0600

    modular coding and web page creation

--- app.js
+++ app.js
@@ -1,17 +1,10 @@
-var profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
+var profileDataArgs = process.argv.slice(2);
+const [Name, github] = profileDataArgs;
 
-const printProfileData = profileDataArr => {
-    // This...
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-      console.log(profileDataArr[i]);
-    }
+  fs.writeFile('index.html', generatePage(Name, github), err => {
+    if (err) throw err;
   
-    console.log('================');
-  
-    // Is the same as this...
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-
-}    
-
-printProfileData(profileDataArgs);
+    console.log('Portfolio complete! Check out index.html to see the output!');
+  });
