c54852cdad4057523dfac762099db095823a1062
Robert C <robert.clarady@gmail.com>
2021-12-20 15:45:17 -0600

    create dynamic html

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const inquirer = require('inquirer');
 
-// const fs = require('fs');
-// const generatePage = require('./src/page-template');
+const fs = require('fs');
+const generatePage = require('./src/page-template');
 
 // const pageHTML = generatePage(name, github);
 
@@ -130,8 +130,14 @@ if (!portfolioData.projects) {
       }); 
 };
  
-  promptUser()
-  .then(promptProject)
-  .then(portfolioData => {
-    console.log(portfolioData);
+promptUser()
+.then(promptProject)
+.then(portfolioData => {
+  const pageHTML = generatePage(portfolioData);
+
+  fs.writeFile('./index.html', pageHTML, err => {
+     if (err) throw new Error(err);
+
+  //   console.log('Page created! Check out index.html in this directory to see it!');
   });
+});
