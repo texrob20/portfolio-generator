a9b2671150589131bef81f43cab394540e27b164
Robert C <robert.clarady@gmail.com>
2021-12-21 11:00:55 -0600

    added promises

--- app.js
+++ app.js
@@ -1,15 +1,7 @@
 const inquirer = require('inquirer');
-
-const fs = require('fs');
 const generatePage = require('./src/page-template');
+const { writeFile, copyFile } = require('./utils/generate-site.js');
 
-// const pageHTML = generatePage(name, github);
-
-// fs.writeFile('./index.html', pageHTML, err => {
-//   if (err) throw err;
-
-//   console.log('Portfolio complete! Check out index.html to see the output!');
-// });
 const promptUser = () => {
     return inquirer.prompt([
       {
@@ -131,13 +123,20 @@ if (!portfolioData.projects) {
 };
  
 promptUser()
-.then(promptProject)
-.then(portfolioData => {
-  const pageHTML = generatePage(portfolioData);
-
-  fs.writeFile('./index.html', pageHTML, err => {
-     if (err) throw new Error(err);
-
-  //   console.log('Page created! Check out index.html in this directory to see it!');
+  .then(promptProject)
+  .then(portfolioData => {
+    return generatePage(portfolioData);
+  })
+  .then(pageHTML => {
+    return writeFile(pageHTML);
+  })
+  .then(writeFileResponse => {
+    console.log(writeFileResponse);
+    return copyFile();
+  })
+  .then(copyFileResponse => {
+    console.log(copyFileResponse);
+  })
+  .catch(err => {
+    console.log(err);
   });
-});
